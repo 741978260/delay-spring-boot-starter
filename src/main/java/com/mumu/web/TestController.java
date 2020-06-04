39c82375f47a8fc76b5adfd8a31ff5aafa635db7
cailin <cailin@vcredit.com>
2020-06-04 12:50:15 +0800

    banner使用输出变量

--- src/main/java/com/mumu/web/TestController.java
+++ src/main/java/com/mumu/web/TestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
-    @Autowired(required = false)
+    @Autowired
     private TestService testService;
 
     @GetMapping("/test")
